56f7325dd3c5cf3757139ffd927c0449b0aea1d1
Erick <eum602@gmail.com>
2019-12-19 03:31:41 -0500

    cleaned some comments

--- health-check.go
+++ health-check.go
@@ -10,28 +10,28 @@ import (
 
 func main() {
 	msg := "0"
-	msg = TestClientUrl("4444")
-	msg = TestNodePort("4040")
+	msg = TestClientUrl("http://127.0.0.1", "4444")
+	msg = TestNodePort("127.0.0.1", "4040")
 	fmt.Println(msg)
 	os.Exit(0)
 }
 
-func TestClientUrl(clientPort string) string {
+func TestClientUrl(url, clientPort string) string {
 	port := flag.String("port", clientPort, "port on localhost to check")
 	flag.Parse()
 
-	resp, err := http.Get("http://127.0.0.1:" + *port + "/upcheck")
+	resp, err := http.Get(url + ":" + *port + "/upcheck")
 	if err != nil || resp.StatusCode != 200 {
-		fmt.Println("dead in client")
+		//fmt.Println("dead in client")
 		return "1"
 	}
 	return "0"
 }
 
-func TestNodePort(nodePort string) string {
-	conn, err := telnet.DialTo("localhost:" + nodePort)
+func TestNodePort(ip, nodePort string) string {
+	conn, err := telnet.DialTo(ip + ":" + nodePort)
 	if err != nil {
-		fmt.Println("NodeUrl not responding")
+		//fmt.Println("NodeUrl not responding")
 		return "1"
 	}
 	conn.Write([]byte("hello world"))
