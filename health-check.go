b3eb5aa592aaaf701245f48e6a0e7f5a4a80aff4
Erick <eum602@gmail.com>
2020-01-13 14:14:18 -0500

    replaced string with bools for verification

--- health-check.go
+++ health-check.go
@@ -9,32 +9,35 @@ import (
 )
 
 func main() {
-	msg := "0"
-	msg = TestClientUrl("http://127.0.0.1", "4444")
-	msg = TestNodePort("127.0.0.1", "4040")
-	fmt.Println(msg)
+	msg1 := TestClientUrl("http://127.0.0.1", "4444")
+	msg2 := TestNodePort("127.0.0.1", "4040")
+	if msg1 || msg2 {
+		fmt.Println("1") //restart
+	} else {
+		fmt.Print("0")
+	}
 	os.Exit(0)
 }
 
-func TestClientUrl(url, clientPort string) string {
+func TestClientUrl(url, clientPort string) bool {
 	port := flag.String("port", clientPort, "port on localhost to check")
 	flag.Parse()
 
 	resp, err := http.Get(url + ":" + *port + "/upcheck")
 	if err != nil || resp.StatusCode != 200 {
 		//fmt.Println("dead in client")
-		return "1"
+		return true
 	}
-	return "0"
+	return false
 }
 
-func TestNodePort(ip, nodePort string) string {
+func TestNodePort(ip, nodePort string) bool {
 	conn, err := telnet.DialTo(ip + ":" + nodePort)
 	if err != nil {
 		//fmt.Println("NodeUrl not responding")
-		return "1"
+		return true
 	}
 	conn.Write([]byte("hello world"))
 	conn.Write([]byte("\n"))
-	return "0"
+	return false
 }
