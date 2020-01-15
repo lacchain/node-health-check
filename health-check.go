2a5c2ab3211054164a74b574930424420da66c72
Erick <eum602@gmail.com>
2020-01-15 17:15:01 -0500

    organized main script to monitor both ports and java process by way of channels

--- health-check.go
+++ health-check.go
@@ -1,43 +1,14 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"github.com/reiver/go-telnet"
-	"net/http"
-	"os"
 )
 
 func main() {
-	msg1 := TestClientUrl("http://127.0.0.1", "4444")
-	msg2 := TestNodePort("127.0.0.1", "4040")
-	if msg1 || msg2 {
-		fmt.Println("1") //restart
-	} else {
-		fmt.Print("0")
+	c := fanIn(executeReadJavaProcess(), testPorts())
+	for <-c != true {
+		fmt.Println(<-c)
 	}
-	os.Exit(0)
-}
 
-func TestClientUrl(url, clientPort string) bool {
-	port := flag.String("port", clientPort, "port on localhost to check")
-	flag.Parse()
-
-	resp, err := http.Get(url + ":" + *port + "/upcheck")
-	if err != nil || resp.StatusCode != 200 {
-		//fmt.Println("dead in client")
-		return true
-	}
-	return false
-}
-
-func TestNodePort(ip, nodePort string) bool {
-	conn, err := telnet.DialTo(ip + ":" + nodePort)
-	if err != nil {
-		//fmt.Println("NodeUrl not responding")
-		return true
-	}
-	conn.Write([]byte("hello world"))
-	conn.Write([]byte("\n"))
-	return false
+	fmt.Println("Restarting", processName, "process...")
 }
